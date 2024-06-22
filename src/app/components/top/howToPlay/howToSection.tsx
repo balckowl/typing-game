66728b633036ad7884d59da150a305d1ab5b074d
takaaki200302@gmail.com <takaaki200302@gmail.com>
2024-06-22 14:05:34 +0900

    style: create toppage style

--- src/app/components/top/howToPlay/howToSection.tsx
+++ src/app/components/top/howToPlay/howToSection.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 const HowToSection = ({title,children,content}:{title:string,children:ReactNode,content:string}) => {
   return (
-    <div className="flex space-x-4">
-      <div className="rounded-full bg-blue-200 p-4">
+    <div className="flex space-x-6">
+      <div className="flex size-[70px] items-center justify-center rounded-full bg-blue-200 p-4">
         {children}
       </div>
       <div className="text-left">
--- src/app/layout/header/header.tsx
+++ src/app/layout/header/header.tsx
@@ -1,11 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import UserButton from "./userButton";
 
 const Header = () => {
   return (
     <div>
       <header className="h-[80px] border-b p-4">
-        <div className="container flex h-full items-center justify-between">
-          <h1 className="text-3xl font-semibold">Tech Type</h1>
+        <div className="container flex h-full max-w-[1200px] items-center justify-between">
+          <Link href="/" className="flex">
+            <Image src="/logo/logo.jpg" alt="logo" width={50} height={50}/>
+            <h1 className="flex items-center text-3xl font-bold">Tech Type</h1>
+          </Link>
           <UserButton />
         </div>
       </header>
