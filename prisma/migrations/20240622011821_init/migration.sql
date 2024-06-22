4112093a6c5db8212c11359d27ee0da0860eb95a
balckowl <shiggyjr1492@gmail.com>
2024-06-22 10:19:40 +0900

    refactor: change-table

---
+++ prisma/migrations/20240622011821_init/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `currTechsNum` on the `User` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Games" DROP CONSTRAINT "Games_userId_fkey";
+
+-- DropIndex
+DROP INDEX "User_name_key";
+
+-- AlterTable
+ALTER TABLE "Games" ALTER COLUMN "techs" SET DEFAULT ARRAY['html', 'css', 'js']::TEXT[],
+ALTER COLUMN "score" SET DEFAULT 0,
+ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "currTechsNum",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "HighScore" SET DEFAULT 0,
+ALTER COLUMN "experiencePoint" SET DEFAULT 0,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "Games" ADD CONSTRAINT "Games_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
